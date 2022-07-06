1180a7e871e7c2e64697b848c6e7725194be360e
TeenAsher <106741447+TeenAsher@users.noreply.github.com>
2022-07-06 12:41:13 +0300

    Made graphs of rolling dice with Matplotlib

---
+++ matplotlib_dice2.py
@@ -0,0 +1,30 @@
+# Using the library Matplotlib to make a die-rolling visualisation
+# TeenAsher
+
+import matplotlib.pyplot as plt
+from die import Die
+
+die = Die()
+
+results = []
+for num_roll in range(1000):
+    result = die.roll()
+    results.append(result)
+
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+x_values = range(1, die.num_sides+1)
+y_values = frequencies
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(x_values, y_values, c='red', linewidth=3)
+
+ax.set_title('Results of rolling one D6 1000 times', fontsize=24)
+ax.set_xlabel('Result', fontsize=14)
+ax.set_ylabel('Frequency of result', fontsize=14)
+
+plt.show()
---
+++ random_walk.py
@@ -0,0 +1,50 @@
+# Studying matplotlib
+
+from random import choice
+
+class RandomWalk:
+    """A class to generate random walks"""
+
+    def __init__(self, num_points=5000):
+        """Initialize attributes of walk"""
+        self.num_points = num_points
+
+        # All walks start at (0, 0)
+        self.x_values = [0]
+        self.y_values = [0]
+
+    def get_step(self):
+        """Calculate the steps"""
+
+        # Decide which direction to go and how far to go in this direction
+        x_direction = choice([1, -1])
+        x_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        x_step = x_direction * x_distance
+
+        y_direction = choice([1, -1])
+        y_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        y_step = y_direction * y_distance
+
+        return x_step, y_step
+
+    def fill_walk(self):
+        """Calculate all the points in the walk"""
+
+        # Keep taking steps until the walk reaches the desired length
+        while len(self.x_values) < self.num_points:
+
+            steps = self.get_step()
+            x_step = int(steps[0])
+            y_step = int(steps[1])
+
+            # Reject moves that go nowhere:
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # Calculate the new position
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+
+            self.x_values.append(x)
+            self.y_values.append(y)
+
