f100d3ccb22dace27fd8b231193acaae0c332c60
TeenAsher <106741447+TeenAsher@users.noreply.github.com>
2022-07-06 12:11:47 +0300

    Made the histogram of rolling three D6

---
+++ 3dice_visual.py
@@ -0,0 +1,31 @@
+# Rolling dice project
+# TeenAsher
+
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
+from die import Die
+
+# Create three D6 dice
+die_1 = Die()
+die_2 = Die()
+die_3 = Die()
+
+results = []
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll() + die_3.roll()
+    results.append(result)
+
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides + die_3.num_sides
+for value in range(3, max_result+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+x_values = list(range(3, max_result+1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result', 'dtick': 1}
+y_axis_config = {'title': 'Frequency of result'}
+my_layout = Layout(title='Results of rolling three D6 dice 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6_d6.html')
